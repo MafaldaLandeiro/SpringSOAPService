13dc157a93c0180836caf4f6fdfb2bff97970a7a
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-03-10 18:00:44 +0000

    Project creation

---
+++ SpringSOAPService/src/main/java/org/SpringSOAPService/App.java
@@ -0,0 +1,13 @@
+package org.SpringSOAPService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
